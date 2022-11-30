d7c29ec7361b35329ea603950acf0a7f1535b749
shoaib patel <100205704+PatelShoaib7@users.noreply.github.com>
2022-11-30 13:26:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 const express = require("express");
 const home = require("./routes/home");
 
+
 // Middlewares
 const app = express();
 app.use(express.json());
 
+
 // Routes
 app.use("/home", home);
+
 
 // connection
 const port = process.env.PORT || 9001;
